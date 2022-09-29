f8e1e347086067111e54d7b63d3506452a209cc7
Dave Stahl <dmstah@gmail.com>
2022-09-29 11:08:01 -0400

    add support for subdirectory processing

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from urllib.request import Request
 from google.cloud import storage
 import csv
 import os
+from pathlib import Path
 
-def convert_csv(bucket_name):
-
+def convert_csv(bucket_name, subdir=""):
     dest_bucket_name = '{}-converted'.format(bucket_name)
     # Create Storage client
-    storage_client = storage.Client()
+    storage_client = storage.Client(project='my-project')
     
     # create reference to source bucket
     src_bucket = storage_client.bucket(bucket_name)
@@ -21,53 +22,60 @@ def convert_csv(bucket_name):
         dest_bucket = storage_client.create_bucket(dest_bucket_name, location='us')
 
     # get list of objects in source bucket
-    blobs = storage_client.list_blobs(bucket_name)
+    blobs = storage_client.list_blobs(bucket_name, prefix=subdir)
 
     # Process all objects in source bucket.
-    # 1. Download From GCS
+    # 1. Download From GCS bucket
     # 2. Convert '|' delimiter to ',' delimiter
     # 3. Write converted file back to local filesystem
     # 4. Upload to GCS
     # 5. Remove both files from local filesystem
     for blob in blobs:
-        print("PROCESSING {}".format(blob.name))
+        if Path(blob.name).suffix == ".csv":
+            process_file(blob.name, src_bucket, dest_bucket)
+        else:
+            print("SKIPPING {}".format(blob.name))
+            
 
-        local_src_file = "/tmp/{}".format(blob.name)
-        local_src_dir = "/tmp/{}".format('/'.join(blob.name.split('/')[0:-1]))
-        if not os.path.exists(local_src_dir):
-            os.makedirs(local_src_dir)
+def process_file(src_file_name, src_bucket, dest_bucket):
+    print("PROCESSING {}".format(src_file_name))
 
-        src_file = src_bucket.blob(blob.name)
-        src_file.download_to_filename(local_src_file)
+    local_src_file = "/tmp/{}".format(src_file_name)
+    local_src_dir = "/tmp/{}".format('/'.join(src_file_name.split('/')[0:-1]))
+    
+    if not os.path.exists(local_src_dir):
+        os.makedirs(local_src_dir)
 
-        # Output
-        local_dest_file = "/tmp/output/{}".format(blob.name)
-        local_dest_dir = "/tmp/output/{}".format('/'.join(blob.name.split('/')[0:-1]))
-        if not os.path.exists(local_dest_dir):
-            os.makedirs(local_dest_dir)
+    src_file = src_bucket.blob(src_file_name)
+    src_file.download_to_filename(local_src_file)
 
-        with open(local_src_file, "r") as file_pipe:
-            with open(local_dest_file, 'w') as file_comma:
-                 csv.writer(file_comma, delimiter=',').writerows(csv.reader(file_pipe, delimiter='|'))
-        
-        dest_blob = dest_bucket.blob(blob.name)
-        dest_blob.upload_from_filename("/tmp/output/{}".format(blob.name))
+    # Output
+    local_dest_file = "/tmp/output/{}".format(src_file_name)
+    local_dest_dir = "/tmp/output/{}".format('/'.join(src_file_name.split('/')[0:-1]))
+    
+    if not os.path.exists(local_dest_dir):
+        os.makedirs(local_dest_dir)
 
-        print("UPLOADED {}".format(dest_blob.name))
+    with open(local_src_file, "r") as file_pipe:
+        with open(local_dest_file, 'w') as file_comma:
+                csv.writer(file_comma, delimiter=',').writerows(csv.reader(file_pipe, delimiter='|'))
+    
+    dest_blob = dest_bucket.blob(src_file_name)
+    dest_blob.upload_from_filename("/tmp/output/{}".format(src_file_name))
 
-        # Remove Temp files
-        os.remove("/tmp/{}".format(blob.name))
-        os.remove("/tmp/output/{}".format(blob.name))
+    print("UPLOADED {}".format(dest_blob.name))
 
+    # Remove Temp files
+    os.remove("/tmp/{}".format(src_file_name))
+    os.remove("/tmp/output/{}".format(src_file_name))
 
-def main(request):
-    if os.environ.get('SRC_BUCKET_NAME'):
-        SRC_BUCKET_NAME = os.environ.get('SRC_BUCKET_NAME')
-    else:
-        SRC_BUCKET_NAME = 'csv-test-1234'
-
-    convert_csv(SRC_BUCKET_NAME)
-    return f'Success!'
+    
+def main():
+    src_bucket_name = "my-bucket"
+    subdir = ""
+    convert_csv(src_bucket_name, subdir)
+    
+    print('Success!')
 
 if __name__ == "__main__":
-    main("foo")
+    main()
